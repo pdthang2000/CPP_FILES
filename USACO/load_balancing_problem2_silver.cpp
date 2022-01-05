30782a8c70adc21a4597a7483ec2cee12cd1ef9c
pdthang2000 <65274725+pdthang2000@users.noreply.github.com>
2022-01-05 10:10:02 +0700

    Update load_balancing_problem2_silver.cpp

--- USACO/load_balancing_problem2_silver.cpp
+++ USACO/load_balancing_problem2_silver.cpp
@@ -45,7 +45,7 @@ void pdthang2000() {
 int main() {
 	ios::sync_with_stdio(0);cin.tie(0);
 	freopen("balancing.in", "r", stdin);
-    freopen("balancing.out", "w", stdout);
+    	freopen("balancing.out", "w", stdout);
 	if (MULTI_TESTS) {
 		ll Q;
 		cin >> Q;
@@ -57,21 +57,6 @@ int main() {
 		pdthang2000();
 	}
 }
-//cout.precision(18);
-//cout << fixed;
-//mp.reserve(2 ^ n); //use for unordered_map
-//mp.max_load_factor(0.1); //use for unordered_map
-//struct custom_hash {
-//	static uint64_t splitmix64(uint64_t x) {
-//		// http://xorshift.di.unimi.it/splitmix64.c
-//		x += 0x9e3779b97f4a7c15;
-//		x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
-//		x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
-//		return x ^ (x >> 31);
-//	}
-// 
-//	size_t operator()(uint64_t x) const {
-//		static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
-//		return splitmix64(x + FIXED_RANDOM);
-//	}
-//};
+/*
+	Explain: Loop all elements, for each v[i], fixed line y, loop to count number of v upper and lower. Then loop again, check state with result.
+*/
